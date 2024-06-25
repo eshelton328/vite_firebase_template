e4a6465d244218ed00f2fa781e52fd256d4d987f
Erik Shelton <erik.shelton328@gmail.com>
2024-06-25 18:44:23 -0500

    Updated to use components

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -1,49 +1,11 @@
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { UserAuth } from '../context/AuthContext';
+import { SignUpForm } from "../features/authentication/index";
 
 const SignUp = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
-
-    const userAuth = UserAuth();
-    const createUser = userAuth?.createUser ?? (() => {});
-
-    const navigate = useNavigate();
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        setError('');
-        try {
-            await createUser(email, password);
-            return navigate('/');
-        } catch (err) {
-            console.log(err)
-            setError((err as Error)?.message);
-        }
-    };
 
     return (
         <>
             <h1>Sign Up</h1>
-            <form onSubmit={handleSubmit}>
-                <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-                <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-                <button type="submit">submit</button>
-                {error && <p>{error}</p>}
-            </form>
-            <p>Already have an account? <a href="/signin">Sign In</a></p>
+            <SignUpForm />
         </>
     );
 };
