a8ee38509aa58b512251a2a6cce4bd1157ec8045
Erik Shelton <erik.shelton328@gmail.com>
2024-06-25 16:14:54 -0500

    Added lazy loading and suspense

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
+import { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AuthContextProvider } from "./context/AuthContext";
 import PublicRoute from "./components/PublicRoute";
 import PrivateRoute from "./components/PrivateRoute";
 import './App.css';
 
-import Home from './pages/Home';
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
-import { AuthContextProvider } from "./context/AuthContext";
+const Home = lazy(() => import('./pages/Home'));
+const SignIn = lazy(() => import('./pages/SignIn'));
+const SignUp = lazy(() => import('./pages/SignUp'));
 
 const App = () => {
 
     return (
         <AuthContextProvider>
             <Router>
-                <Routes>
-                    <Route path="/signin" element={<PublicRoute page={<SignIn />} />}/>
-                    <Route path="/signup" element={<PublicRoute page={<SignUp />} />} />
-                    <Route path="/" element={<PrivateRoute page={<Home />} />}/>
-                </Routes>
+                <Suspense fallback={<div></div>}>
+                    <Routes>
+                        <Route path="/signin" element={<PublicRoute page={<SignIn />} />}/>
+                        <Route path="/signup" element={<PublicRoute page={<SignUp />} />} />
+                        <Route path="/" element={<PrivateRoute page={<Home />} />}/>
+                    </Routes>
+                </Suspense>
             </Router>
         </AuthContextProvider>
     );
