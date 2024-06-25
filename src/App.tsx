62e0f5d3e5552e545f8f8fef665ed16e777d97ee
Erik Shelton <erik.shelton328@gmail.com>
2024-06-25 14:48:25 -0500

    Updated with context and react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
+
+import Home from './pages/Home';
+import Login from "./pages/SignIn";
+import Register from "./pages/SignUp";
+import { AuthContextProvider, UserAuth } from "./context/AuthContext";
 
 const App = () => {
-  return (
-    <>
-      Hello World
-    </>
-  )
+    const user = UserAuth();
+    if (!user) {
+        console.log("no user")
+    }
+
+    return (
+        <AuthContextProvider>
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Home />}/>
+                    <Route path="/signin" element={<Login />}/>
+                    <Route path="/signup" element={<Register />}/>
+                </Routes>
+            </Router>
+        </AuthContextProvider>
+    );
 }
 
-export default App
+export default App;
